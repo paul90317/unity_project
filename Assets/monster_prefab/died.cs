cf27aeef0452648de74663246405cf66e41dc773
paul <paul2309825@gmail.com>
2020-12-11 14:06:12 +0800

    untiy_project

---
+++ Assets/monster_prefab/died.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class died : MonoBehaviour {
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.transform.tag == "Player")
+        {
+            transform.parent.gameObject.SendMessage("removeMonster");
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/pau1234/script/walker/unit.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class unit : MonoBehaviour {
+    public GameObject floor;
+    public GameObject wall;
+    // Use this for initialization
+    bool flag;
+	void Awake() {
+        flag = transform.parent.gameObject.GetComponent<walker>().isVertical();
+        GameObject[] t = new GameObject[3];
+        t[0] = Instantiate<GameObject>(wall, transform);
+        t[0].transform.localPosition = new Vector3(0, 2, 0);
+        t[1] = Instantiate<GameObject>(floor, transform);
+        t[1].transform.localPosition = new Vector3(0, 0, 0);
+        t[1].GetComponent<SpriteRenderer>().size = new Vector2(1, 3);
+        t[2] = Instantiate<GameObject>(wall, transform);
+        t[2].transform.localPosition = new Vector3(0, -2, 0);
+        if (flag)
+        {
+            t[0].transform.Rotate(0, 0, -90);
+            t[2].transform.Rotate(0, 0, -90);
+        }
+    }
+}
