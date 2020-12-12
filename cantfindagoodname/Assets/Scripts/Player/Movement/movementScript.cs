c7c2e018181c51c0b2b41bf20fb9239e0d40ed87
cantfindagoodname <f74087060@gs.ncku.edu.tw>
2020-12-12 19:26:48 +0800

    Added directory for paul90317 and cantfindagoodname
    - yet to merge programs

---
+++ cantfindagoodname/Assets/Scripts/Player/Movement/movementScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movementScript : MonoBehaviour {
+
+    private float moveSpeed = 50f;
+    private Rigidbody2D rb;
+
+    private Vector2 movement;
+    private void Awake()
+    {
+        rb = GameObject.FindGameObjectWithTag("Player").GetComponent<Rigidbody2D>();
+    }
+
+    private void Update () {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+	}
+
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+    }
+}
