9e58c4836cbdd664aa3e475a4a30c4c7502e8790
paul <paul2309825@gmail.com>
2020-12-12 21:40:46 +0800

    restore

---
+++ cantfindagoodname/Assets/Scripts/Enemy/DetectPlayer.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CircleCollider2D))]
+public class DetectPlayer : MonoBehaviour {
+    public Vector2 playerPos;
+
+    private void Awake()
+    {
+        playerPos = transform.position;
+    }
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            Vector2 vectorToPlayer = collision.transform.position - transform.position;
+            RaycastHit2D hit = Physics2D.Raycast(transform.position, vectorToPlayer, 30f, LayerMask.GetMask("Wall") | LayerMask.GetMask("Player"));
+            if (hit && hit.collider.gameObject.CompareTag("Player"))
+            {
+                playerPos = collision.transform.position;
+            }
+        }
+    }
+}
