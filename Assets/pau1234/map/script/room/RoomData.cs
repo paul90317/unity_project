be46fdff292bb118785cafa63f4306e5c6f36c7d
justinwu1220 <z891220z@gmail.com>
2020-12-20 20:12:10 +0800

    add 3 enemy texture

---
+++ Assets/pau1234/map/script/room/RoomData.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "newRoomData", menuName = "Room/RoomData")]
+public class RoomData : ScriptableObject {
+
+    public TextAsset roomMap;
+    public int[] monsterWave;
+}
